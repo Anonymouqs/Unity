e77ec16ada676ebda90a91f78577f726bcf643d5
Anonymouqs <jehbar.ibarra@gmail.com>
2017-09-25 11:11:49 -0500

    Hi

---
+++ Unity/2D project assginment/TerariaRipoff/TerrariaRipoff/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D rigid;
+	public float jumpSpeed;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask ground;
+	public bool grounded;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, ground); 
+			
+		if (Input.GetAxisRaw ("Horizontal") > 0f) 
+		{
+			rigid.velocity = new Vector3 (moveSpeed, rigid.velocity.y, 0f);
+		} 
+		else if (Input.GetAxisRaw ("Horizontal") < 0f) {
+			rigid.velocity = new Vector3 (-moveSpeed, rigid.velocity.y, 0f);
+		} 
+		else 
+		{
+			rigid.velocity = new Vector3 (0f, rigid.velocity.y, 0f);
+		}
+		if (Input.GetButtonDown("Jump"))
+		{
+			rigid.velocity = new Vector3 (rigid.velocity.x, jumpSpeed, 0f);
+		}
+
+	}
+}
---
+++ Unity/New Unity Project/Assets/Scripts/PC/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D rigid;
+	public float jumpSpeed;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask ground;
+	public bool grounded;
+	private float playerScale;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D>();
+		playerScale = gameObject.transform.localScale.y;
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, ground); 
+		print (grounded);
+		if (Input.GetAxisRaw ("Horizontal") > 0f) 
+		{
+			rigid.velocity = new Vector3 (moveSpeed, rigid.velocity.y, 0f);
+			transform.localScale = new Vector3(playerScale,playerScale,playerScale);
+			print("Player.Move.Right");
+		} 
+		else if (Input.GetAxisRaw ("Horizontal") < 0f) {
+			rigid.velocity = new Vector3 (-moveSpeed, rigid.velocity.y, 0f);
+			transform.localScale = new Vector3(-playerScale,playerScale,playerScale);
+			print("Player.Move.Left");
+		} 
+		else 
+		{
+			rigid.velocity = new Vector3 (0f, rigid.velocity.y, 0f);
+		}
+		if (Input.GetButtonDown("Jump") && grounded)
+		{
+			rigid.velocity = new Vector3 (rigid.velocity.x, jumpSpeed, 0f);
+			print("Player.Move.Jump");
+		}
+		anim.SetFloat ("Speed", Mathf.Abs (rigid.velocity.x));
+		anim.SetBool ("Grounded", grounded);
+
+	}
+}
