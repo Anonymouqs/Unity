0a1f6cfb63e0f673fa6f3cf9c372a7733151631a
Ibarra <jehbar.ibarra@nths-adm14.belton.isd>
2017-09-25 10:12:09 -0500

    derp

---
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject player = GameObject.Find ("Player");
+	private Vector3 target;
+	private Vector3 cameraPos;
+	public float cameraZ;  //This is the Z value for the Camera
+	public float moveRate;//Overall Interpolation Speed to move to player
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cameraPos = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,gameObject.transform.position.z);
+		GameObject.Find ("Player");
+		target = new Vector3 (player.transform.position.x, player.transform.position.y, cameraZ);
+
+	}
+}
