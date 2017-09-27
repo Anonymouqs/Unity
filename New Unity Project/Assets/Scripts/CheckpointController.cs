10d6cba57e55a76429fe62621ceaaec55380a178
Ibarra <jehbar.ibarra@nths-adm14.belton.isd>
2017-09-27 10:34:01 -0500

    Hello

---
+++ New Unity Project/Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour {
+	public Sprite flagClosed;
+	public Sprite flagOpen;
+	private SpriteRenderer theSpriteRenderer;
+	// Use this for initialization
+	void Start () {
+		theSpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onTrigger2DEnter(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			theSpriteRenderer.sprite = flagOpen;
+		}
+	}
+}
