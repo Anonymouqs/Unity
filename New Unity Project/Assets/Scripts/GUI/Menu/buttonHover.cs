ceccb3079f4ce0c2526f54e6c84d0ebbe3e99ed6
Ibarra <jehbar.ibarra@nths-adm14.belton.isd>
2017-10-02 10:35:45 -0500

    Menu work

---
+++ New Unity Project/Assets/Scripts/GUI/Menu/buttonHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonHover : MonoBehaviour {
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnMouseOver()
+	{
+		anim.SetFloat("mouseOver",(true));
+	}
+	void OnMouseExit()
+	{
+		anim.SetFloat("mouseOver",false);
+	}
+}
